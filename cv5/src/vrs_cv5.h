d9e317d22345fd020a82304d1981c257864a20f5
9DumonT9 <vitek.peter@gmail.com>
2016-10-28 09:36:07 +0200

    Cvicenie 5

---
+++ cv5/src/vrs_cv5.h
@@ -0,0 +1,16 @@
+/*
+ * vrs_cv5.h
+ *
+ *  Created on: 18. 10. 2016
+ *      Author: Peter Vitek
+ */
+
+#ifndef VRS_CV5_H_
+#define VRS_CV5_H_
+
+
+void NVIC_init(void);
+void ADC_init(void);
+void UART_init(void);
+
+#endif /* VRS_CV5_H_ */
